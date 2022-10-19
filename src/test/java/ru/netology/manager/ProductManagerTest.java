bbb037cb04a96b2283f5fc4c88dfe95735517ece
Роман Панченко <romanx23@yandex.ru>
2022-10-19 19:34:31 +0300

    Product manager v2

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -47,14 +47,6 @@ class ProductManagerTest {
     }
 
     @Test
-    void shouldFindBookByAuthorIfExists() {
-        String textToFind = "Мураками";
-        Product[] expected = new Product[]{book1};
-        Product[] actual = manager.searchBy(textToFind);
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
     void shouldNotFindBookByAuthorIfNoExists() {
         String textToFind = "Маяковский";
         Product[] expected = new Product[]{};
@@ -74,14 +66,6 @@ class ProductManagerTest {
     void shouldNotFindSmartphoneByNameIfNoExists() {
         String textToFind = "Redmi";
         Product[] expected = new Product[]{};
-        Product[] actual = manager.searchBy(textToFind);
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    void shouldFindSmartphoneByProducerIfExists() {
-        String textToFind = "Apple";
-        Product[] expected = new Product[]{phone1};
         Product[] actual = manager.searchBy(textToFind);
         assertArrayEquals(expected, actual);
     }
